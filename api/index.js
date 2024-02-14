031245bf95df8544e4274819cb1f87147f08efb1
uddinmudassir <uddinmudassir10@gmail.com.com>
2024-02-14 21:55:13 +0530

    middleware and error handler

--- api/index.js
+++ api/index.js
@@ -25,3 +25,17 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRoute);
 app.use('/api/auth', authRoute);
+
+//----------------------Middleware---------------------//
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal server error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
+//----------------------End-Middleware---------------------//
